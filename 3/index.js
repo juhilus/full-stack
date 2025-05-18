cb30cfa5d019206c9bd5eb8131b855f3ecefd8f8
Juha <juha.ponkkonen@aalto.fi>
2025-05-18 17:43:06 +0300

    3.9

--- 3/index.js
+++ 3/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
+const { request } = require('express')
 
-
+app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 morgan.token('bodyreq', request => {
     return JSON.stringify(request.body)
 })
@@ -94,8 +97,7 @@ app.post('/api/persons', (request, response) => {
     response.status(201).json(personN)
   })
 
-const PORT = 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
-
+  const PORT = process.env.PORT || 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
