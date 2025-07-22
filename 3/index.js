a22a4c9bf2e1abacc26c61413f3011e8b6f2b6bc
Juha <juha.ponkkonen@aalto.fi>
2025-07-22 19:24:03 +0300

    3.15-3.18

--- 3/index.js
+++ 3/index.js
@@ -18,36 +18,50 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
   Person.find({}).then(result => {
     response.json(result)
   })
+  .catch(error => next(error))
 })
 
-app.get('/info', (request, response) => {
+app.get('/info', (request, response, next) => {
     const curT = new Date().toString()
-    const persN = Persons.length
-    const info = `
-    <p>Phonebook has info for ${persN} people</p>
-    <p>${curT}</p>`
-    response.send(info)
-  })
+    Person.find({}).then(result => {
+      const info = `
+       <p>Phonebook has info for ${result.length} people</p>
+       <p>${curT}</p>`
+       response.send(info)
+    }).catch(error => next(error))
+   
+    
+})
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id).then(person => {
       if(person){
         response.json(person)
       }else{
         response.status(404).end()
       }
-    })
+    }).catch(error => next(error))
   })
 
-app.delete('/api/persons/:id', (request, response) => {
-const id = request.params.id
-persons = Persons.filter(person => person.id !== id)
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id).then(result => {
+    response.status(204).end()
+  }).catch(error => next(error))
 
-response.status(204).end()
+})
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+  Person.findByIdAndUpdate(request.params.id, person, { new: true }).then(uPerson => {
+    response.json(uPerson)
+  }).catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -72,7 +86,16 @@ app.post('/api/persons', (request, response) => {
 
   })
 
-  const PORT = process.env.PORT
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name == 'CastError'){
+      return response.status(400).send({ error: 'invalid id'})
+  }
+    next(error)
+  }
+app.use(errorHandler)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
