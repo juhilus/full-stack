3fa1b7221d3e86c0e8ccdebc9e768b6dfbd29d91
Juha <juha.ponkkonen@aalto.fi>
2025-09-14 16:42:51 +0300

    4.15 - 4.16

---
+++ 4/controllers/users.js
@@ -0,0 +1,32 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.post('/', async (request, response) => {
+  const { username, name, password } = request.body
+  if(password == undefined || username == undefined){
+    return response.status(400).json({ error: 'Setting password and username is required'})
+  } else if(password.length < 3 || username.length < 3){
+      return response.status(400).json({ error: 'Password and username must be more than 3 chars long'})
+  } else {
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  })
+
+  const savedUser = await user.save()
+
+  response.status(201).json(savedUser)
+  }
+})
+
+usersRouter.get('/', async (request, response) => {
+    const users = await User.find({})
+    response.json(users)
+  })
+
+module.exports = usersRouter
--- 4/tests/blog_api.test.js
+++ 4/tests/blog_api.test.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const helper = require('./test_helper')
+const bcrypt = require('bcrypt')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 const { default: expect } = require('expect')
 
 const api = supertest(app)
@@ -15,7 +17,7 @@ beforeEach(async () => {
 })
 
 
-describe('When initial notes exists', () => {
+describe('When initial blogs exists', () => {
 test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
@@ -143,6 +145,103 @@ test('Single blog can be edited', async () => {
 
 
 })
+})
+
+describe('when there is initially one user at db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'juhilus',
+      name: 'Juhilus Ponks',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
+
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(result.body.error).toContain('expected `username` to be unique')
+
+    expect(usersAtEnd).toEqual(usersAtStart)
+  })
+
+  test('creation fails with proper statuscode and message if username is less than 3 chars', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'ei',
+      name: 'Superuser',
+      password: 'salainen',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(result.body.error).toContain("Password and username must be more than 3 chars long")
+
+    expect(usersAtEnd).toEqual(usersAtStart)
+  })
+
+  test('creation fails with proper statuscode and message if password is less than 3 chars', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'rot',
+      name: 'Superuser',
+      password: 'ei',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(result.body.error).toContain("Password and username must be more than 3 chars long")
+
+    expect(usersAtEnd).toEqual(usersAtStart)
+  })
+
 })
 
 afterAll(async () => {
